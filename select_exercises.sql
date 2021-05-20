906b22c252dd97cf7e9c542d5e45be752284bfc7
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-05-20 02:23:59 -0500

    feat: update

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,13 @@
 USE codeup_test_db;
 
-SELECT ''
+SELECT 'The name of all albums by pink floyd' AS "";
+SELECT * FROM albums WHERE artist_name = "Pink Floyd";
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club band was released' AS "";
+SELECT * FROM albums WHERE release_date = 1967;
+SELECT 'The genre for Nevermind' AS "";
+SELECT * FROM albums WHERE
+SELECT 'Which albums were released in the 1990''s' AS "";
+SELECT
+SELECT 'Which albums had less than 20 million certified sales' AS "";
+SELECT
+SELECT 'All the albums with a genre of "Rock"' AS "";
