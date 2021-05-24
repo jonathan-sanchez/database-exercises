3d744c6a832819963b3056cd6e3d4300aa6765aa
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-05-24 11:58:03 -0500

    feat: updated

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,25 +1,29 @@
 USE employees;
+#1 Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
 
 SELECT CONCAT(first_name, ' ', last_name)
 FROM employees
-WHERE last_name IN ('Irene', 'Vidya', 'Maya') ORDER BY first_name;
+WHERE last_name LIKE 'E%e' ORDER BY last_name;
 
-SELECT first_name, last_name
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+#2 Find all employees born on Christmas:
 
-SELECT last_name
+SELECT first_name, last_name, birth_date
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name AND first_name;
+WHERE MONTH(birth_date) = 12 AND DAYOFMONTH(birth_date) = 25;
 
-SELECT first_name, last_name
-FROM employees
-WHERE last_name LIKE '%e%' ORDER BY emp_no;
+#3 Find all employees hired in the 90s and born on Christmas:
 
-SELECT first_name, last_name
+SELECT first_name, last_name, hire_date, birth_date
 FROM employees
-WHERE first_name OR ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC;
+WHERE hire_date BETWEEN '1990-01-01' AND '2000-12-31' AND MONTH(birth_date) = 12 AND DAYOFMONTH(birth_date) = 25;
 
-SELECT first_name, last_name
+#4 Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+
+SELECT first_name, last_name, hire_date, birth_date
 FROM employees
-WHERE last_name LIKE 'e%' ORDER BY emp_no DESC;
+WHERE hire_date BETWEEN '1990-01-01' AND '2000-12-31' AND MONTH(birth_date) = 12 AND DAYOFMONTH(birth_date) = 25 ORDER BY birth_date, hire_date DESC;
+
+#5 For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
+
+SELECT first_name, last_name, DATEDIFF(CURDATE(), hire_date) AS 'days employed'
+FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '2000-12-31' AND MONTH(birth_date) = 12 AND DAYOFMONTH(birth_date) = 25;
