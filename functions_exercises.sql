837eaeff3a19bb7830587ecb6fffaa82943b13c3
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-05-23 23:07:14 -0500

    feat: update

---
+++ functions_exercises.sql
@@ -0,0 +1,25 @@
+USE employees;
+
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE last_name IN ('Irene', 'Vidya', 'Maya') ORDER BY first_name;
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+
+SELECT last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name AND first_name;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%e%' ORDER BY emp_no;
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name OR ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'e%' ORDER BY emp_no DESC;
--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,4 +1,16 @@
 USE employees;
 
-SELECT last_name
+-- #1
+SELECT DISTINCT last_name
 FROM employees
+ORDER BY last_name DESC LIMIT 10;
+
+-- #2
+SELECT emp_no
+FROM salaries
+ORDER BY salary DESC LIMIT 5;
+
+-- #3
+SELECT emp_no
+FROM salaries
+ORDER BY salary DESC LIMIT 5 OFFSET 45;
